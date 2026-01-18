4fd5850b6dc348dec06d07527b8f4988d3772c05
Goran_Milosavljevic <gmilosavljevic@inforce.io>
2026-01-18 15:37:52 -0500

    Add quote summary functionality and refactor related steps Added method for selecting radio buttons in base page Added locator.press("Tab") in method type_text to simulate real user behavior Changed some of the fill methods with custom type_text method

--- ui/pages/base_page.py
+++ ui/pages/base_page.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import Page
 
 class BasePage:
@@ -19,7 +21,21 @@ class BasePage:
         locator = self.wait_visible(selector)
         locator.click()
         locator.fill(value)
+        locator.press("Tab")
+
 
     def click_element(self, selector: str):
         locator = self.wait_clickable(selector)
         locator.click()
+
+    def answer_question(self, group_name: str, answer: str):
+        if not answer:
+            return
+        # This is th method for radio buttons(i will need to add comments later)
+        answer = str(answer).strip()
+        group = self.page.get_by_role("radiogroup", name=re.compile(group_name, re.I))
+        radio = group.get_by_label(re.compile(f"^{answer}$", re.I))
+
+        radio.dispatch_event("click")
+
+
